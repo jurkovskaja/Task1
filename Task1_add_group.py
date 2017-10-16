5a97bb3b1f556f1a01ec4b04be1327d46de7aa85
skorohodova <skorohodova@ftc.ru>
2017-10-16 22:35:08 +0700

    В тесте использован вспомогательный класс для инициализации тестовых данных и для передачи этих данных во вспомогательные методы

--- Task1_add_group.py
+++ Task1_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
+from group import Group
 
 def is_alert_present(wd):
     try:
@@ -31,19 +31,19 @@ class Task1_add_group(unittest.TestCase):
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("%s" % name)
+        wd.find_element_by_name("group_name").send_keys("%s" % group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("%s" % header)
+        wd.find_element_by_name("group_header").send_keys("%s" % group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("%s" % footer)
+        wd.find_element_by_name("group_footer").send_keys("%s" % group.footer)
         # submit groop creation
         wd.find_element_by_name("submit").click()
 
@@ -53,23 +53,24 @@ class Task1_add_group(unittest.TestCase):
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def Task1_add_group(self):
+    def test_Task1_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin",password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="Task1", header="Task1", footer="Task1")
+        self.create_group(wd, Group(name="Task1", header="Task1", footer="Task1"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
-    def Task1_add_empty_group(self):
+    def test_Task1_add_empty_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="", header="", footer="")
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
+
     def tearDown(self):
         self.wd.quit()
 
