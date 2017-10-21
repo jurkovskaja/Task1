0b1100ef9797f4e5bd0905f750c4333e03ead902
skorohodova <skorohodova@ftc.ru>
2017-10-21 19:55:39 +0700

    Добавлено удаление группы

--- fixture/group.py
+++ fixture/group.py
@@ -26,6 +26,7 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
