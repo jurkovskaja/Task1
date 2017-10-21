1c3926baa84df73928e1a53bc73640b616746be1
skorohodova <skorohodova@ftc.ru>
2017-10-21 19:42:27 +0700

    Из теста вынесена фикстура в отдельный глобальный файл

--- test/Task2_add_group.py
+++ test/Task2_add_group.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
 from model.group import Group
-
-# init fixture
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    # an indication of how fixture should be destroyed
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_Task2_add_group(app):
     app.session.login(username="admin",password="secret")
