faec444981ed0de98a8a6cd5c017b229bc37bbe6
skorohodova <skorohodova@ftc.ru>
2017-10-23 21:24:38 +0700

    Наведен порядок в тестах. Удалены тесты, относящиеся к первому заданию. Тесты переименованы

--- conftest.py
+++ conftest.py
@@ -1,8 +1,9 @@
 import pytest
 from fixture.application import Application
 
+
 # init fixture
-@pytest.fixture (scope="session")
+@pytest.fixture(scope="session")
 def app(request):
     fixture = Application()
     # an indication of how fixture should be destroyed
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-def test_Task2_add_group(app):
+def test_add_group(app):
     app.session.login(username="admin",password="secret")
     app.group.create(Group(name="Task2", header="Task2", footer="Task2"))
     app.session.logout()
 
-def test_Task2_add_empty_group(app):
+def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
