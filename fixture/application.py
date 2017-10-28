92df157f13c21179af66883d8cfea1d1641862c9
skorohodova <skorohodova@ftc.ru>
2017-10-28 15:56:04 +0700

    Добавлен вызов метода  self.open_home_page()

--- fixture/application.py
+++ fixture/application.py
@@ -8,10 +8,11 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.open_home_page()
 
     def is_valid(self):
         try:
