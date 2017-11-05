b7eeb4444a6ae8815663b8a62399f404e94ca8e5
skorohodova <skorohodova@ftc.ru>
2017-11-05 23:04:48 +0700

    Попытка реализовать проверку в виде сравнения сортированных списков в тесте добавления контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,12 +4,15 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="qwee", middlename="qwere", lastname="qwerqwetqe", nickname="dfgfdg",
+    cont = Contact(firstname="5558", middlename="qwere", lastname="qwerqwetqe", nickname="dfgfdg",
                               title="dfgnhgf", company="sdfsdfsdf", address="qwerwreqer", home="11", mobile_phone="22",
                               work_phone="33", fax="444", email="5555", email2="666", email3="777", homepage="sadffasd",
                               address2="1wrtrewer", phone2="phone2", notes="sdfgnvbxfgsdf", date="option[18]",
                               month="option[12]", byear="123", aday="option[31]", amonth="option[2]", ayear="1231")
-    app.contact.create(contact)
+    app.contact.create(cont)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(cont)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
 
