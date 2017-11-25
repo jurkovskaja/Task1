3c1b4871e128af00459d691fc7af86fed4c5336c
skorohodova <skorohodova@ftc.ru>
2017-11-25 22:17:46 +0700

    Удаление контакта. Проверка через БД. Неверно считается кол-во контактов после удлаения

--- fixture/group.py
+++ fixture/group.py
@@ -49,6 +49,19 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']"%id).click()
+
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # submit delition
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def change_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
